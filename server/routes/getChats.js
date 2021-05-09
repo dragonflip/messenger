9e3daa3708fb38a43ad5b6c71d1f4f6c5a3b3f2a
YuraA30 <61061567+YuraA30@users.noreply.github.com>
2021-05-09 14:53:36 +0300

    Maybe fixed chats

--- server/routes/getChats.js
+++ server/routes/getChats.js
@@ -5,10 +5,14 @@ const db = require("../config/db");
 
 router.get("/:token", async (req, res) => {
   let [chats] = await db.query(
-      `SELECT users.firstname, users.lastname, messages.message, messages.sent_date, messages.has_read FROM users INNER JOIN messages ON 
-      users.id = messages.from_id OR users.id = messages.to_id WHERE users.token = ${req.params.token} 
-      AND messages.sent_date = (SELECT MAX(sent_date) FROM messages WHERE from_id = (SELECT id FROM users WHERE token = ${req.params.token}) 
-      OR to_id = (SELECT id FROM users WHERE token = ${req.params.token}))`
+
+
+      `SELECT messages.from_id, users.firstname, users.lastname, messages.message, messages.sent_date, messages.has_read, 
+      (SELECT COUNT(message) FROM messages WHERE to_id = (SELECT id FROM users WHERE token = ${req.params.token}) 
+      AND has_read = 0) AS count FROM users INNER JOIN messages ON users.id = messages.from_id OR users.id = messages.to_id 
+      WHERE users.token = ${req.params.token} AND messages.sent_date = (SELECT MAX(sent_date) FROM messages 
+      WHERE from_id = (SELECT id FROM users WHERE token = ${req.params.token}) OR to_id = (SELECT id FROM users 
+      WHERE token = ${req.params.token}))`
   )
 
   res.json(chats);
