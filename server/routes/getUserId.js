62e818525061fb72765517301dac5bbee0976803
Vadym Tsummer <62544653+icenfame@users.noreply.github.com>
2021-05-10 19:15:55 +0300

    Added online status

--- server/routes/getUserId.js
+++ server/routes/getUserId.js
@@ -2,12 +2,21 @@ const { Router } = require("express");
 const router = Router();
 const db = require("../config/db");
 
+const moment = require("moment");
+
 router.get("/:token", async (req, res) => {
   let [user_id] = await db.query(
     `SELECT id FROM users WHERE token = '${req.params.token}'`
   );
 
   if (user_id.length > 0) {
+    // SET online, (temp solution)
+    await db.query(
+      `UPDATE users SET was_online = "${moment().unix() + 60}" WHERE token = "${
+        req.params.token
+      }"`
+    );
+
     res.json(user_id[0]);
   } else {
     res.json({ user_id: null });
