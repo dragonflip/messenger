e420a89aebcb6c82fff40dce19a76d556a45e399
Vadym Tsummer <62544653+icenfame@users.noreply.github.com>
2021-06-10 22:19:07 +0300

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -6,7 +6,7 @@ const db = mysql
     host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_USER,
+    database: process.env.DB_NAME,
   })
   .promise();
 
